5f833c50292091a10be3cc9d6aab10a8799dd473
Sergey Krasovskiy <xhunter74@gmail.com>
2015-05-27 23:52:02 +0300

    Added spell checker

--- app/src/main/java/com/eleks/voiceassistant/voiceassistantpoc/activity/MainActivity.java
+++ app/src/main/java/com/eleks/voiceassistant/voiceassistantpoc/activity/MainActivity.java
@@ -1,14 +1,22 @@
 package com.eleks.voiceassistant.voiceassistantpoc.activity;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.textservice.SentenceSuggestionsInfo;
+import android.view.textservice.SpellCheckerSession;
+import android.view.textservice.SpellCheckerSession.SpellCheckerSessionListener;
+import android.view.textservice.SuggestionsInfo;
+import android.view.textservice.TextInfo;
+import android.view.textservice.TextServicesManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,9 +30,10 @@ import com.nuance.nmdp.speechkit.SpeechError;
 import com.nuance.nmdp.speechkit.SpeechKit;
 
 import java.util.List;
+import java.util.Locale;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SpellCheckerSessionListener {
 
     private static final int LISTENING_DIALOG = 0;
     private static final int SPEECH_REQUEST_CODE = 777;
@@ -35,6 +44,8 @@ public class MainActivity extends ActionBarActivity {
     private Recognizer mCurrentRecognizer;
     private Handler _handler = null;
     private boolean _destroyed;
+    private EditText mCorrectedSpeechResult;
+    private SpellCheckerSession mScs;
 
     public MainActivity() {
         super();
@@ -43,6 +54,17 @@ public class MainActivity extends ActionBarActivity {
 
     static SpeechKit getSpeechKit() {
         return sSpeechKit;
+    }
+
+    private void startSpellCheck(String text) {
+        if (mScs != null) {
+            if (isSentenceSpellCheckSupported()) {
+                mScs.getSentenceSuggestions(new TextInfo[]{new TextInfo(text)}, 1);
+
+            }else{
+                mScs.getSuggestions(new TextInfo(text), 1);
+            }
+        }
     }
 
     @Override
@@ -139,6 +161,7 @@ public class MainActivity extends ActionBarActivity {
                 for (int i = 0; i < count; i++) {
                     resultStr += "[" + results.getResult(i).getScore() + "] " +
                             results.getResult(i).getText() + "\n";
+                    startSpellCheck(results.getResult(i).getText());
                 }
                 mSpeechResult.setText(resultStr);
                 // for debugging purpose: printing out the speechkit session id
@@ -160,6 +183,7 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View view) {
                 mSpeechResult.setText("");
+                mCorrectedSpeechResult.setText("");
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                         RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -187,12 +211,14 @@ public class MainActivity extends ActionBarActivity {
                 showDialog(LISTENING_DIALOG);
                 mListeningDialog.setStoppable(false);
                 mSpeechResult.setText("");
+                mCorrectedSpeechResult.setText("");
                 mCurrentRecognizer = getSpeechKit().createRecognizer(
                         Recognizer.RecognizerType.Search, Recognizer.EndOfSpeechDetection.Short,
                         "en_US", _listener, _handler);
                 mCurrentRecognizer.start();
             }
         });
+        mCorrectedSpeechResult = (EditText) findViewById(R.id.correctedSpeechResult);
     }
 
     @Override
@@ -201,6 +227,51 @@ public class MainActivity extends ActionBarActivity {
         if (sSpeechKit != null) {
             sSpeechKit.release();
             sSpeechKit = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final TextServicesManager tsm = (TextServicesManager) getSystemService(
+                Context.TEXT_SERVICES_MANAGER_SERVICE);
+        mScs = tsm.newSpellCheckerSession(null, Locale.getDefault(), MainActivity.this, false);
+    }
+
+    private boolean isSentenceSpellCheckSupported() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+    }
+
+    @Override
+    public void onGetSuggestions(final SuggestionsInfo[] arg0) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arg0.length; ++i) {
+            // Returned suggestions are contained in SuggestionsInfo
+            final int len = arg0[i].getSuggestionsCount();
+            for (int j = 0; j < len; ++j) {
+                sb.append(arg0[i].getSuggestionAt(j));
+            }
+            sb.append("\n");
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String text = mCorrectedSpeechResult.getText().toString();
+                text += sb.toString();
+                mCorrectedSpeechResult.setText(text);
+            }
+        });
+    }
+
+    @Override
+    public void onGetSentenceSuggestions(SentenceSuggestionsInfo[] arg0) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arg0.length; ++i) {
+            final int len = arg0[i].getSuggestionsCount();
+            for (int j = 0; j < len; ++j) {
+                //sb.append(arg0[i].getSuggestionAt(j));
+            }
+            sb.append("\n");
         }
     }
 
@@ -214,6 +285,7 @@ public class MainActivity extends ActionBarActivity {
                 String spokenText = "";
                 for (String result : results) {
                     spokenText += result + "\n";
+                    startSpellCheck(result);
                 }
                 mSpeechResult.setText(spokenText);
             }
