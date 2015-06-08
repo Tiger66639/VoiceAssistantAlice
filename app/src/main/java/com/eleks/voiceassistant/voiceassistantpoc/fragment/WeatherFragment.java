703023821402a8824889a0f145e6ac88ab8ecdbb
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-08 20:58:08 +0300

    Added weather icons translations.

--- app/src/main/java/com/eleks/voiceassistant/voiceassistantpoc/fragment/WeatherFragment.java
+++ app/src/main/java/com/eleks/voiceassistant/voiceassistantpoc/fragment/WeatherFragment.java
@@ -54,6 +54,73 @@ public class WeatherFragment extends Fragment {
         cityName.setTypeface(mTypeface);
         TextView weatherIcon = (TextView) mFragmentView.findViewById(R.id.weather_icon);
         weatherIcon.setTypeface(mTypeface);
+        weatherIcon.setText(
+                getWeatherIcon(mWeatherModel.forecast.simpleForecast.forecastDays[0].iconName));
+    }
+
+    private String getWeatherIcon(String iconName) {
+        iconName = iconName.toLowerCase();
+        int resourceId = 0;
+        switch (iconName) {
+            case "chanceflurries":
+                resourceId = R.string.chanceflurries;
+                break;
+            case "chancerain":
+                resourceId = R.string.chancerain;
+                break;
+            case "chancesleet":
+                resourceId = R.string.chancesleet;
+                break;
+            case "chancesnow":
+                resourceId = R.string.chancesnow;
+                break;
+            case "chancetstorms":
+                resourceId = R.string.chancetstorms;
+                break;
+            case "clear":
+                resourceId = R.string.clear;
+                break;
+            case "cloudy":
+                resourceId = R.string.cloudy;
+                break;
+            case "flurries":
+                resourceId = R.string.flurries;
+                break;
+            case "fog":
+                resourceId = R.string.fog;
+                break;
+            case "hazy":
+                resourceId = R.string.hazy;
+                break;
+            case "mostlycloudy":
+                resourceId = R.string.mostlycloudy;
+                break;
+            case "mostlysunny":
+                resourceId = R.string.mostlysunny;
+                break;
+            case "partlycloudy":
+                resourceId = R.string.partlycloudy;
+                break;
+            case "partlysunny":
+                resourceId = R.string.partlysunny;
+                break;
+            case "sleet":
+                resourceId = R.string.sleet;
+                break;
+            case "rain":
+                resourceId = R.string.rain;
+                break;
+            case "snow":
+                resourceId = R.string.snow;
+                break;
+            case "sunny":
+                resourceId = R.string.sunny;
+                break;
+            case "tstorms":
+                resourceId = R.string.tstorms;
+                break;
+        }
+        return getString(resourceId);
     }
 
     private void getExtras() {
