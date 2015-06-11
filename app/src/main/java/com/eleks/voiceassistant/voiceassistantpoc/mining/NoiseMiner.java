6bf1cafb8cff467269bfe7ee721555fd573fa416
Sergey Krasovskiy <xhunter74@gmail.com>
2015-06-11 16:14:08 +0300

    Improved miner.

--- app/src/main/java/com/eleks/voiceassistant/voiceassistantpoc/mining/NoiseMiner.java
+++ app/src/main/java/com/eleks/voiceassistant/voiceassistantpoc/mining/NoiseMiner.java
@@ -8,7 +8,7 @@ import android.content.Context;
 public class NoiseMiner implements ITextMiner {
 
     private static final String[] NOISE_WORDS = {"in", "of", "the", "a", "please", "for", "what",
-            "would", "be", "on", "is"};
+            "would", "be", "on", "is", "hey", "will"};
 
     @Override
     public WordHolder[] investigate(Context context, WordHolder[] words) {
